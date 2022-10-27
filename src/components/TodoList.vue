c936045cb5fb060fd39a96cd246f902662d473d0
seokyHan <sseok1979@naver.com>
2022-10-27 11:11:16 +0900

    first commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      todoItems: []
+    }
+  },
+  created: function() {
+    if(localStorage.length > 0) {
+      for(let i = 0; i < localStorage.length; i++){
+        this.todoItems.push(localStorage.key(i))
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
