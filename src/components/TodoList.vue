07bac539a340572dcd9f45ad72b033a42af81a55
seokyHan <sseok1979@naver.com>
2023-02-09 16:28:01 +0900

    add edit function

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,34 +3,69 @@
     <transition-group name="list" tag="ul">
       <li
         v-for="(todoItem, index) in this.storedTodoItems"
-        :key="todoItem.item"
+        :key="todoItem.id"
         class="shadow"
       >
-        <i
-          class="checkBtn fas fa-check"
-          v-bind:class="{ checkBtnCompleted: todoItem.completed }"
-          v-on:click="toggleComplete({ todoItem, index })"
-        >
-        </i>
-        <span v-bind:class="{ textCompleted: todoItem.completed }">
-          {{ todoItem.item }}
-        </span>
-        <span class="removeBtn" v-on:click="removeTodo({ todoItem, index })">
-          <i class="fas fa-trash-alt"></i>
-        </span>
+        <template v-if="editingIndex !== index">
+          <i
+            class="checkBtn fas fa-check"
+            :class="{ checkBtnCompleted: todoItem.completed }"
+            @click="toggleComplete({ todoItem, index })"
+          >
+          </i>
+          <span :class="{ textCompleted: todoItem.completed }">
+            {{ todoItem.item }}
+          </span>
+          <span class="editBtn" @click="startEditing(index)">
+            <i class="fas fa-edit"></i>
+          </span>
+          <span class="removeBtn" @click="removeTodo({ todoItem, index })">
+            <i class="fas fa-trash-alt"></i>
+          </span>
+        </template>
+        <template v-else>
+          <div>
+            <input ref="inputField" v-model="editValue" />
+          </div>
+          <span
+            class="modifyTodo"
+            @click="editTodo({ todoItem, newValue: editValue, index })"
+          >
+            <i class="fas fa-check"></i>
+          </span>
+          <span class="removeBtn" @click="stopEditing">
+            <i class="fas fa-times"></i>
+          </span>
+        </template>
       </li>
     </transition-group>
+
+    <Modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">
+        변경!
+        <i class="closeModalBtn fas fa-times" @click="showModal = false"></i>
+      </h3>
+      <div slot="body">
+        변경이 완료되었습니다!.
+      </div>
+    </Modal>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapMutations } from "vuex";
+import Modal from "./common/Modal.vue";
 
 export default {
   data() {
     return {
-      isEditing: false
+      editingIndex: null,
+      editValue: "",
+      showModal: false
     };
+  },
+  components: {
+    Modal
   },
   computed: {
     ...mapGetters(["storedTodoItems"])
@@ -39,7 +74,30 @@ export default {
     ...mapMutations({
       removeTodo: "removeOneItem",
       toggleComplete: "toggleOneItem"
-    })
+    }),
+    startEditing(index) {
+      this.editingIndex = index;
+      this.editValue = this.storedTodoItems[index].item;
+      this.$nextTick(() => {
+        this.$refs.inputField[0].focus();
+        this.$refs.inputField[0].selectionStart = this.$refs.inputField[0].value.length;
+      });
+    },
+    stopEditing() {
+      this.editingIndex = null;
+    },
+    editTodo({ todoItem, newValue: editValue, index }) {
+      const editCheck = this.storedTodoItems[index].item === this.editValue;
+      if (!editCheck) {
+        this.$store.commit("editOneItem", {
+          todoItem,
+          newValue: editValue,
+          index
+        });
+        this.showModal = !this.showModal;
+      }
+      this.stopEditing();
+    }
   }
 };
 </script>
@@ -66,6 +124,14 @@ li {
   color: #62acde;
   margin-right: 5px;
 }
+.modifyTodo {
+  margin-left: auto;
+  margin-right: -77.5%;
+}
+.editBtn {
+  margin-left: auto;
+  margin-right: -90%;
+}
 .removeBtn {
   margin-left: auto;
   color: #de4343;
@@ -76,6 +142,10 @@ li {
 .textCompleted {
   text-decoration: line-through;
   color: #b3adad;
+}
+.closeModalBtn {
+  float: right;
+  color: #42b983;
 }
 
 /* 리스트 아이템 트랜지션 */
--- src/store/modules/todoApp.js
+++ src/store/modules/todoApp.js
@@ -2,9 +2,9 @@ const storage = {
   fetch() {
     const arr = [];
     if (localStorage.length > 0) {
-      for (let i = 0; i < localStorage.length; i++) {
-        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
-          arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+      for (const key of Object.keys(localStorage)) {
+        if (key !== "loglevel:webpack-dev-server") {
+          arr.push(JSON.parse(localStorage.getItem(key)));
         }
       }
     }
@@ -12,20 +12,20 @@ const storage = {
   }
 };
 
-const findPrimaryKey = payload => {
-  const keys = Object.keys(localStorage);
+// const findPrimaryKey = payload => {
+//   const keys = Object.keys(localStorage);
 
-  const filteredKeys = keys.filter(
-    key => key !== "loglevel:webpack-dev-server"
-  );
+//   const filteredKeys = keys.filter(
+//     key => key !== "loglevel:webpack-dev-server"
+//   );
 
-  const findKey = filteredKeys.find(key => {
-    const { item } = JSON.parse(localStorage.getItem(key)) || {};
-    return item === payload.todoItem.item;
-  });
+//   const findKey = filteredKeys.find(key => {
+//     const { item } = JSON.parse(localStorage.getItem(key)) || {};
+//     return item === payload.todoItem.item;
+//   });
 
-  return findKey;
-};
+//   return findKey;
+// };
 
 const state = {
   todoItems: storage.fetch()
@@ -40,6 +40,7 @@ const getters = {
 const mutations = {
   addOneItem(state, payload) {
     const obj = {
+      id: state.todoItems.length + 1,
       completed: false,
       item: payload
     };
@@ -48,19 +49,23 @@ const mutations = {
   },
 
   removeOneItem(state, payload) {
-    const primaryKey = findPrimaryKey(payload);
-
-    localStorage.removeItem(primaryKey);
+    localStorage.removeItem(payload.todoItem.id);
     state.todoItems.splice(payload.index, 1);
   },
 
+  editOneItem(state, payload) {
+    state.todoItems[payload.index].item = payload.newValue;
+
+    localStorage.removeItem(payload.todoItem.id);
+    localStorage.setItem(payload.todoItem.id, JSON.stringify(payload.todoItem));
+  },
+
   toggleOneItem(state, payload) {
-    const primaryKey = findPrimaryKey(payload);
     state.todoItems[payload.index].completed = !state.todoItems[payload.index]
       .completed;
 
-    localStorage.removeItem(primaryKey);
-    localStorage.setItem(primaryKey, JSON.stringify(payload.todoItem));
+    localStorage.removeItem(payload.todoItem.id);
+    localStorage.setItem(payload.todoItem.id, JSON.stringify(payload.todoItem));
   },
 
   clearAllItem(state) {
